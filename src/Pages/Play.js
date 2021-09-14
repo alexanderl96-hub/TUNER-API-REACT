d4aa354ec5e3cc39649f8322c7bbccd8b1192338
Alexander <alexanderl96@yahoo.com>
2021-09-14 01:10:59 -0400

    adding mp3 audioplayer

--- src/Pages/Play.js
+++ src/Pages/Play.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import ReactPlayer from 'react-player'
+import Playlist from 'react-mp3-player'
+// import tracks from "../mp3Player"
 import {useEffect, useState} from 'react'
 import { Link, useParams } from "react-router-dom"
 import axios from "axios"
 import {apiURL} from "../Back-end/apiURl"
+import './AudioPlayer.module.css'
 
 const API_DTBASE = apiURL()
 
 export default function Play() {
     const {id} = useParams()
     const [songs, setSong]= useState({})
+
 
     useEffect(()=>{
         axios.get(`${API_DTBASE}/songs/${id}`).then((res)=>{
@@ -25,7 +28,25 @@ export default function Play() {
                     <button className="songsbt">Back</button>
              </Link>
             <h2 className='player'>Aqui empieza todo</h2>
-            <ReactPlayer url={songs} className='player'/>
+            {/* <Playlist tracks={songs} className='player'/> */}
+            <div className="audioPlayer">
+                <audio src='https://music.youtube.com/watch?v=hCK-N8cKtns&feature=share' preload="metadata"></audio>
+                <button>back 10</button>
+                <button>play / pause</button>
+                <button>forward 10</button>
+
+              {/* current time */}
+              <div className="countCurrent">0:00</div>
+
+              {/* progress bar */}
+              <div>
+                  <input type="range"  style={{ width: '1000px'}}/>
+              </div>
+
+              {/* duration */}
+              <div className="fullCount">3:45</div>
+  
+            </div>
         </div>
     )
 }
---
+++ src/mp3Player.js
@@ -0,0 +1,17 @@
+// import Tedejolibre.mp3 from "../src/mp3_sorce/Tedejolibre.mp3";
+// const tracks = [
+//   {
+//     img: "https://icon-library.net/images/music-icon-transparent/music-icon-transparent-11.jpg",
+//     name: "MP3",
+//     desc: "Description 1",
+//     src: 'Tedejolibre.mp3',
+//   },
+//   {
+//     img: "https://icon-library.net/images/music-icon-transparent/music-icon-transparent-11.jpg",
+//     name: "MP3 #2",
+//     desc: "Description 2",
+//     src: "Audio2.mp3",
+//   },
+// ];
+
+// export default tracks;
